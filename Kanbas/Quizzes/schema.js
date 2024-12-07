acc375e5e1c7815e340c3ee5e3649fa9cfa3a0b0
Jenny Nguyen <jennayx14@yahoo.com>
2024-12-06 22:38:24 -0800

    updated quiz schema

--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -1,34 +1,49 @@
 import mongoose from "mongoose";
+
 const quizSchema = new mongoose.Schema({
-    quiz: {type: mongoose.Schema.Types.ObjectId, ref: "QuizModel"},
     course: { type: mongoose.Schema.Types.ObjectId, ref: "CourseModel" },
-    title: String,
-    description: String,
+    title: { type: String, required: true },
+    quiz: {type: mongoose.Schema.Types.ObjectId, ref: "QuizModel"},
+   
+    description: { type: String, required: true },
     assignedTo: String,
-    quiz_type: String,
-    points: String,
-    group: String,
-    shuffle_answers: String,
-    time_limit: String,
-    multiple_attempts: String,
+    quiz_type: { 
+        type: String, 
+        enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"], 
+        default: "Graded Quiz" 
+      },
+    points: { type: Number, required: true, default: 0 },
+    group:  { 
+        type: String, 
+        enum: ["Quizzes", "Exams", "Assignments", "Project"], 
+        default: "Quizzes" 
+      },
+    shuffle_answers: { type: Boolean, default: true },
+    time_limit: { type: Number, default: 20 },
+    multiple_attempts:  { type: Boolean, default: false },
     show_answers: String,
     access_code: String,
-    one_question: String,
-    webcam: String,
-    lock_questions: String,
-    dueDate: String,
-    available: String,
-    availableUntil: String,
+    one_question: { type: Boolean, default: true },
+    webcam: { type: Boolean, default: false },
+    lock_questions: { type: Boolean, default: false },
+    dueDate:  { type: Date, required: true },
+    available:  { type: Date, required: true },
+    availableUntil:  { type: Date, required: true },
 
     questions:[
     {
-        type: String, 
+        type:{
+            type: String, 
             enum: ["MC", "TF", "FB"],
-            question: String,
-            options: [],
-         
+            required: true
+        },
+        question: { type: String, required: true }, 
+        options: { type: [String], required: true },
+        correct_answer: { type: String }, 
+        points: { type: Number, required: true },
+       
     },
-]
+],
 },
   { collection: "quizzes" }
 );
